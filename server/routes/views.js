029e72de8b3c8151f7f29eb696a62afe17d36834
Chris Rabuse <rab233@live.com>
2016-06-18 15:13:31 -0500

    Move views to the src folder

--- server/routes/views.js
+++ server/routes/views.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 // Partials
 router.get('/:view/partial/:partial', (req, res) => {
   // Set path to the partial file
-  let partial = path.join(__dirname, '../../views/' + req.params.view + '/partials/' + req.params.partial + '.pug');
+  let partial = path.join(__dirname, '../../src/' + req.params.view + '/views/partials/' + req.params.partial + '.pug');
 
   // Check if the file exists
   fs.access(partial, fs.R_OK, function(err) {
@@ -21,20 +21,20 @@ router.get('/:view/partial/:partial', (req, res) => {
 
 // Admin Page
 router.get('/admin', (req, res) => {
-  res.render(path.join(__dirname, '../../views/admin/app.pug'));
+  res.render(path.join(__dirname, '../../src/admin/views/app.pug'));
 });
 
 /*************************       KEEP AT BOTTOM       *************************/
 
 // Admin catchall for client side routing
 router.get('/admin/*', (req, res) => {
-  res.render(path.join(__dirname, '../../views/admin/app.pug'));
+  res.render(path.join(__dirname, '../../src/admin/views/app.pug'));
 });
 
 
 // App catchall for client side routing
 router.get('/*', (req, res) => {
-  res.render(path.join(__dirname, '../../views/app/app.pug'));
+  res.render(path.join(__dirname, '../../src/client/views/app.pug'));
 });
 
 module.exports = router;
