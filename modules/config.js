3a79f95d0ee4167b1b98721aff18a09258676b8c
Chris Rabuse <rab233@live.com>
2016-10-09 19:09:22 -0500

    Revert "Add config file"
    This reverts commit c42b849b8902ad493bc886d08a965ee70a5ab7ad.

--- modules/config.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-// Read the configuration file
-var file = fs.readFileSync('../config.json');
-
-// Parse file contents to JSON
-var config = JSON.parse(configFile);
-
-module.exports = config;
